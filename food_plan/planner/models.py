11c4764b69b306e76a21038fe3237ed06ad35551
Yuriy Mikhailov <apofiz2036@bk.ru>
2025-04-23 08:14:13 +0500

    create models: Allergy, DietType, UserProfile

--- food_plan/planner/models.py
+++ food_plan/planner/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class 
+
+class Allergy(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Аллергия')
+
+    def __str__(self):
+        return self.name
+
+
+class DietType(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Тип диеты')
+
+    def __str__(self):
+        return self.name
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE,
+        verbose_name='Пользователь'
+    )
+    allergies = models.ManyToManyField(
+        Allergy,
+        blank=True,
+        verbose_name='Аллергии'
+    )    
+    diet_type = models.ForeignKey(
+        DietType,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Тип диеты'
+    )
+    budget_limit = models.DecimalField(
+        max_digits=10, decimal_places=2,
+        verbose_name='Ограничение по стоимости'
+    )
+
+    count_of_persons = models.IntegerField(
+        default=1,
+        verbose_name='Количество персон'
+    )
+    breakfast = models.BooleanField(verbose_name='Завтрак')
+    lunch = models.BooleanField(verbose_name='Обед')
+    dinner = models.BooleanField(verbose_name='Ужин')
+    dessert = models.BooleanField(verbose_name='Десерт')
+    subscription_end_date = models.DateField(
+        verbose_name='Дата окончания подписки'
+    )  # при подсчёте тарифа прибавляем дни к конечной дате
+
+    def __str__(self):
+        return self.user.username
