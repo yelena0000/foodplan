35b20559b5d6aba343fdf1f0fd4bb54d7774e363
yelena0000 <kirillnek07081999@gmail.com>
2025-04-24 01:39:12 +0300

    Create views for authentication (authorization is not fully implemented yet)

--- food_plan/urls.py
+++ food_plan/urls.py
@@ -8,8 +8,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('auth/', views.auth, name='auth'),
-    path('registration/', views.registration, name='registration'),
     path('lk/', views.lk, name='lk'),
     path('order/', views.order, name='order'),
     path('card/', views.card, name='card'),
+    path('registration/', views.register_view, name='registration'),
+    path('logout/', views.logout_view, name='logout'),
+    path('order/', views.order_view, name='order'),
+    path('lk/', views.lk_view, name='lk'),
 ]
